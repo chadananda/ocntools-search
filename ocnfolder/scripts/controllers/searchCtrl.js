5169673586710dfa0b5a5e0d0a3ba182d3651d94
oym <y.ostapchuk@bytesarea.com>
2015-09-23 15:06:43 +0300

    Read each file from json file and save them to documents db

--- ocnfolder/scripts/controllers/searchCtrl.js
+++ ocnfolder/scripts/controllers/searchCtrl.js
@@ -1,8 +1,42 @@
 var app = angular.module('onctoolsSearchApp');
 
-app.controller('SearchCtrl', function($scope, $http) {
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+app.controller('SearchCtrl', function($scope, $q, $http) {
     $http.get('books.json').then(function(result) {
         $scope.contents = result.data;
         $scope.indent = 0;
+
+        var documentDb = new PouchDB('documents');
+        var sentenceDb = new PouchDB('sentences');
+
+        var promises = (function readFiles(data, directory) {
+            directory = directory || '';
+            var path = directory + (directory ? '/' : '');
+            var promises = [];
+            data.files.forEach(function(f) {
+                var filename = path + f.name;
+                if (filename.endsWith('.pdf') || filename.endsWith('.docx')) return;
+                promises.push($http.get(filename, {transformResponse: undefined}).then(function(result) {
+                    documentDb.put({
+                        _id: f.id.toString(),
+                        url: filename,
+                        timestamp: f.modified
+                    });
+                }));
+            });
+            data.directories.forEach(function(d) {
+                promises = promises.concat(readFiles(d.contents, path + d.name));
+            });
+            return promises;
+        }($scope.contents, '../sample'));
+
+        $q.all(promises).then(function() {
+            documentDb.allDocs({include_docs: true}).then(function(result) {
+                console.log(result);
+            })
+        });
     });
 });
